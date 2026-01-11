c297256a2a816a409d19872623543bfb309ef57d
Kaminskyi <kaminskyiwladyslaw@gmail.com>
2026-01-11 17:31:14 +0100

    changed names of classes and syntax in Truck.java

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -6,7 +6,6 @@ public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator is started");
     }
-
     @Override
     public void stopWork() {
         System.out.println("Excavator is stopped");
